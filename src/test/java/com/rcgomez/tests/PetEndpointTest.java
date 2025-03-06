c6eaa5c1db7f6ca1815d8283273017f46fe9cfb0
Roberto Gomez <roberto.gomez02@globant.com>
2025-03-06 11:30:42 -0500

    feat: Test for pet follows schema

--- src/test/java/com/rcgomez/tests/PetEndpointTest.java
+++ src/test/java/com/rcgomez/tests/PetEndpointTest.java
@@ -3,6 +3,7 @@ package com.rcgomez.tests;
 import com.rcgomez.config.TestRunner;
 import com.rcgomez.model.PetDTO;
 import com.rcgomez.request.RequestBuilder;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -28,5 +29,14 @@ public class PetEndpointTest extends TestRunner {
         for (PetDTO pet: retrievedList) {
             Assert.assertEquals(pet.getStatus(),"available");
         }
+    }
+
+    @Test(testName = "GET to a random PET id follows pet schema")
+    public void findAPet() {
+
+        Response response = RequestBuilder.getRequest(getBaseUrl(),"/pet/1");
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("pet_schema.json"));
     }
 }
